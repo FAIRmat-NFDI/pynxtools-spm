64375900b6586a481364b605cbd37588e789d84e
Rubel <rubel.mozumder@outlook.com>
2024-12-19 09:02:09 +0100

    Remove customised examples.

--- src/pynxtools_spm/nomad/entrypoints.py
+++ src/pynxtools_spm/nomad/entrypoints.py
@@ -19,47 +19,47 @@
 
 from nomad.config.models.plugins import ExampleUploadEntryPoint
 
-sts_default_example = ExampleUploadEntryPoint(
-    title="Scanning Tunneling Spectroscopy (STS)",
-    category="SPM experiments examples (with default configuration)",
-    description="""This example (with default configuration) presents the capabilities of the NOMAD platform to store standardized Scanning Tunneling Spectroscopy (sts) data.""",
-    plugin_package="pynxtools_spm",
-    resources=["nomad/examples/sts/STSDefaultExample/*"],
-)
+# sts_default_example = ExampleUploadEntryPoint(
+#     title="Scanning Tunneling Spectroscopy (STS)",
+#     category="FAIRmat Examples",
+#     description="""This example (with default configuration) presents the capabilities of the NOMAD platform to store standardized Scanning Tunneling Spectroscopy (sts) data.""",
+#     plugin_package="pynxtools_spm",
+#     resources=["nomad/examples/sts/STSDefaultExample/*"],
+# )
 sts_example_with_customization = ExampleUploadEntryPoint(
     title="Scanning Tunneling Spectroscopy (STS)",
-    category="SPM experiments examples (with customization options)",
-    description="""This example (with customization options) presents the capabilities of the NOMAD platform to store standardized Scanning Tunneling Spectroscopy (sts) data""",
+    category="FAIRmat Examples",
+    description="""This example (config.json file is an optional input) presents the capabilities of the NOMAD platform to store standardized Scanning Tunneling Spectroscopy (sts) data""",
     plugin_package="pynxtools_spm",
     resources=["nomad/examples/sts/STSExampleWithCustomization/*"],
 )
 
-stm_default_example = ExampleUploadEntryPoint(
-    title="Scanning Tunneling Microscopy (STM)",
-    category="SPM experiments examples (with default configuration)",
-    description="""This example (with default configuration) presents the capabilities of the NOMAD platform to store standardized Scanning Tunneling Microscopy (stm) data.""",
-    plugin_package="pynxtools_spm",
-    resources=["nomad/examples/stm/STMDefaultExample/*"],
-)
+# stm_default_example = ExampleUploadEntryPoint(
+#     title="Scanning Tunneling Microscopy (STM)",
+#     category="FAIRmat Examples",
+#     description="""This example (with default configuration) presents the capabilities of the NOMAD platform to store standardized Scanning Tunneling Microscopy (stm) data.""",
+#     plugin_package="pynxtools_spm",
+#     resources=["nomad/examples/stm/STMDefaultExample/*"],
+# )
 stm_example_with_customization = ExampleUploadEntryPoint(
     title="Scanning Tunneling Microscopy (STM)",
-    category="SPM experiments examples (with customization options)",
-    description="""This example (with customization options) presents the capabilities of the NOMAD platform to store standardized Scanning Tunneling Microscopy (stm) data""",
+    category="FAIRmat Examples",
+    description="""This example (config.json file is an optional input) presents the capabilities of the NOMAD platform to store standardized Scanning Tunneling Microscopy (stm) data""",
     plugin_package="pynxtools_spm",
     resources=["nomad/examples/stm/STMExampleWithCustomization/*"],
 )
 
-afm_default_example = ExampleUploadEntryPoint(
-    title="Atomic Force Microscopy (AFM)",
-    category="SPM experiments examples (with default configuration)",
-    description="""This example (with default configuration) presents the capabilities of the NOMAD platform to store standardized Atomic Force Microscopy (afm) data.""",
-    plugin_package="pynxtools_spm",
-    resources=["nomad/examples/afm/AFMDefaultExample/*"],
-)
+# afm_default_example = ExampleUploadEntryPoint(
+# title="Atomic Force Microscopy (AFM)",
+# category="FAIRmat Examples",
+# description="""This example (with default configuration) presents the capabilities of the NOMAD platform to store standardized Atomic Force Microscopy (afm) data.""",
+# plugin_package="pynxtools_spm",
+# resources=["nomad/examples/afm/AFMDefaultExample/*"],
+# )
 afm_example_with_customization = ExampleUploadEntryPoint(
     title="Atomic Force Microscopy (AFM)",
-    category="SPM experiments examples (with customization options)",
-    description="""This example (with customization options) presents the capabilities of the NOMAD platform to store standardized Atomic Force Microscopy (afm) data""",
+    category="FAIRmat Examples",
+    description="""This example (config.json file is an optional input) presents the capabilities of the NOMAD platform to store standardized Atomic Force Microscopy (afm) data""",
     plugin_package="pynxtools_spm",
     resources=["nomad/examples/afm/AFMExampleWithCustomization/*"],
 )
