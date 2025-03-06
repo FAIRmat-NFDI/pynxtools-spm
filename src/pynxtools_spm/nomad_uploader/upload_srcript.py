5bc248924d36b2e818f712040d6ad355331214e9
Rubel <rubel.mozumder@outlook.com>
2025-03-06 09:43:11 +0100

    Special Commit for local example to test the tools.

--- src/pynxtools_spm/nomad_uploader/upload_srcript.py
+++ src/pynxtools_spm/nomad_uploader/upload_srcript.py
@@ -4,7 +4,8 @@ from pynxtools_spm.nomad_uploader.reader_config_setup import (
 )
 from datetime import datetime, timedelta
 from pathlib import Path
-from dataclasses import asdict
+from typing import Optional
+from dataclasses import asdict, dataclass
 import time
 
 from pynxtools_spm.nomad_uploader.nomad_upload_api import (
@@ -19,132 +20,189 @@ from pynxtools_spm.nomad_uploader.files_movers import copy_directory_structure
 
 from multiprocessing import Process, Lock, Queue
 
-current_dir = Path(__file__).parent
-# File extensions
-available_file_extensions = (".dat", ".sxm")
-# Peseudo extension if upload is sucessfull
-done_extensions = (".done",)
-nomad_url = "https://nomad-lab.eu/prod/v1/test/api/v1/"
-username = "Mozumder"
-password = ""
-parallel_processing = True
-time_for_cron_job = 10 * 60  # seconds
-copy_file_elsewhere = False  # Copy files to another location
-# Mimic sucessfully uploaded raw file with `done` extension
-mimic = True
-exp_src_path = (
-    current_dir.parent.parent.parent
-    / "tests"
-    / "nomad_upload"
-    / "data"
-    / "src_dir_tree"
-)  # Where experiment files are stored after expriment by default
-exp_dst_path = Path("/tmp")
-modify_upload_metadata = False  # Modify metadata of the upload
-publish_to_nomad = False  # Publish the upload to the central NOMAD repository
 
-# List of SPMConvertInputParameters objects to run reader on each object
-SPM_PARAMS_OBJ_L = []
+debug = True
+
+
+@dataclass
+class NOMADSettings:
+    url_protocol: str = "https"
+    url_domain: str = "nomad-lab.eu"
+    url_version: str = "prod/v1/api/v1/"
+    url: str = f"{url_protocol}://{url_domain}/{url_version}"
+    username: str = "Mozumder"
+    password: str = ""
+    token: str = ""
+    # If metadata of the upload file will be modified
+    modify_upload_metadata: bool = False
+    # If the upload will be published to the central NOMAD database
+    publish_to_nomad: bool = False
+
+
+@dataclass
+class DataProcessingSettings:
+    raw_file_exts: tuple[str, ...] = (".dat", ".sxm")
+    single_batch_processing_time: int = 20 * 60  # seconds
+    parallel_processing: bool = True
+    # Considered as a root directory for the experiment files
+    src_dir: Optional[Path] = (
+        current_dir.parent.parent.parent / "tests" / "data" / "cronJobTest"
+    )
+    copy_file_elsewhere: bool = False
+    # Destination directory for the experiment files, if files are moved
+    # to another location after processing is done, needs `copy_file_elsewhere`
+    # to be True
+    dst_dir: Optional[Path] = "/tmp"
+    # create and empty file if upload is sucessfull
+    create_pseudo_file: bool = True
+    # Extension to the empty file if upload is sucessfull
+    pseudo_exts: str = ".done"
+    # List of SPMConvertInputParameters objects to run reader on each object
+    spm_params_obj_l = []
+    sts_eln = (
+        src_dir
+        / ".."
+        / "nanonis"
+        / "sts"
+        / "version_gen_5e_with_described_nxdata"
+        / "eln_data.yaml"
+    )
+    sts_config = ""
+    stm_eln = (
+        src_dir
+        / ".."
+        / "nanonis"
+        / "stm"
+        / "version_gen_4_5_with_described_nxdata"
+        / "eln_data.yaml"
+    )
+    stm_config = ""
+    afm_eln = (
+        src_dir
+        / ".."
+        / "nanonis"
+        / "afm"
+        / "version_gen_4_with_described_nxdata"
+        / "eln_data.yaml"
+    )
+    afm_config = ""
+
 
 def get_unprocessed_files(src_dir: Path) -> list:
     """Filter out the files that are not processed yet.
 
-    An input file `file.dat` will be denoted as `file.dat.done` after
-    successful upload to NOMAD.
+    E.g. an input file `file.dat` will be denoted as `file.dat.done` after
+    successful upload to NOMAD. So, it always checks for the raw files
+    and if the corresponding `.done` file is not present, it will be
+    considered as unprocessed.
     """
     process_status_map = {}
-    for file in src_dir.iterdir():
-        if file.is_file() and file.suffix in available_file_extensions:
+    for file in src_dir.glob("**/*.*"):
+        if file.is_file() and file.suffix in DataProcessingSettings.raw_file_exts:
             process_status_map[file] = False
-    for file in src_dir.iterdir():
-        if file.is_file() and file.suffix in done_extensions:
-            # Remove the pseudo extension
+    for file in src_dir.glob("**/*.*"):
+        if file.is_file() and file.suffix == DataProcessingSettings.pseudo_exts:
+            # Remove extra pseudo extension
             process_status_map[file.with_suffix("")] = True
 
-    def get_non_processed_file(file, is_processed):
+    def filter_non_processed_file(arg):
+        file, is_processed = arg[0], arg[1]
         if not is_processed:
             return file
 
-    return list(filter(get_non_processed_file, process_status_map.items()))
+    return list(map(filter_non_processed_file, process_status_map.items()))
 
 
-def set_and_store_prepare_parameters(file):
-    # sts exeriment
-    sts_eln_file = exp_src_path / "sts" / "eln_data.yaml"
-    sts_config_file = ""
-
-    # stm exeriment
-    stm_eln_file = exp_src_path / "stm" / "eln_data.yaml"
-    stm_config_file = ""
-
-    # afm exeriment
-    afm_eln_file = ""
-    afm_config_file = ""
-
-    file_str = str(file)
+def set_and_store_prepared_parameters(file: Path):
     params_obj = None
-    if file_str.endswith(".dat"):
+    if file.suffix == ".dat":
         params_obj = SPMConvertInputParameters(
             input_file=(file,),
-            eln=sts_eln_file,
+            eln=DataProcessingSettings.sts_eln,
             expriement_type="sts",
-            config=sts_config_file,
+            config=DataProcessingSettings.sts_config,
             nxdl="NXsts",
             raw_extension="dat",
             create_zip=True,
         )
-    elif file_str.endswith(".sxm"):
+    elif file.suffix == ".sxm":
         params_obj = SPMConvertInputParameters(
             input_file=(file,),
-            eln=stm_eln_file,
+            eln=DataProcessingSettings.stm_eln,
             expriement_type="stm",
-            config=stm_config_file,
+            config=DataProcessingSettings.stm_config,
             nxdl="NXstm",
             raw_extension="sxm",
             create_zip=True,
         )
-    elif file_str.endswith(".sxm"):
+    elif file.suffix == ".sxm":
         params_obj = SPMConvertInputParameters(
             input_file=(file,),
-            eln=afm_eln_file,
+            eln=DataProcessingSettings.afm_eln,
             expriement_type="afm",
-            config=afm_config_file,
+            config=DataProcessingSettings.afm_config,
             nxdl="NXafm",
             raw_extension="sxm",
             create_zip=True,
         )
     if not params_obj:
         return
-    # global SPM_PARAMS_OBJ_L
-    SPM_PARAMS_OBJ_L.append(params_obj)
+    DataProcessingSettings.spm_params_obj_l.append(params_obj)
 
 
 if __name__ == "__main__":
-    token = get_authentication_token(nomad_url, username, password)
-    if not token:
-        print("Authentication failed!")
+    NOMADSettings.token = get_authentication_token(
+        NOMADSettings.url, NOMADSettings.username, NOMADSettings.password
+    )
+    if not NOMADSettings.token:
+        print("AuthenticationFailed: Token is required to upload files.")
         exit(1)
+    if (
+        not DataProcessingSettings.src_dir
+        or not DataProcessingSettings.src_dir.is_dir()
+    ):
+        print("Process Exits: Source directory is required to process the files.")
+        exit(1)
+
     # In case user wants to store the files elsewhere
-    if copy_file_elsewhere:
+    if DataProcessingSettings.copy_file_elsewhere:
+        if (
+            not DataProcessingSettings.dst_dir
+            or not DataProcessingSettings.dst_dir.is_dir()
+        ):
+            print(
+                "Porcess Exits: Destination directory is required to store the files."
+            )
+            exit(1)
+
         copy_directory_structure(
-            exp_src_path,
-            exp_dst_path,
-            run_action_on_files=set_and_store_prepare_parameters,
+            DataProcessingSettings.src_dir,
+            DataProcessingSettings.dst_dir,
+            run_action_on_files=set_and_store_prepared_parameters,
         )
     else:
-        file_list = get_unprocessed_files(exp_src_path)
+        file_list = get_unprocessed_files(DataProcessingSettings.src_dir)
+        # Prepare the input parameters for the SPM reader for each file
+        _ = [
+            set_and_store_prepared_parameters(file)
+            for file in file_list
+            if (file and file.is_file())
+        ]
+        print("Process Info: Files to process", file_list)
 
-        _ = [set_and_store_prepare_parameters(file) for file in file_list]
+    if debug:
+        print(
+            "Debug: Files copied over to the destination directory.",
+            DataProcessingSettings.spm_params_obj_l,
+        )
+    # TODO: We Need scrutiny observation for parallel processing
 
-    print("Files copied over to the destination directory.", SPM_PARAMS_OBJ_L)
-    # TODO: We can use parallel processing here
-
-    if not parallel_processing:
+    if not DataProcessingSettings.parallel_processing:
         pass
 
     lock = Lock()
     results_q = Queue()
-    time_out = int(time_for_cron_job / 3)  # seconds
+    time_out = int(DataProcessingSettings.single_batch_processing_time / 3)  # seconds
 
     def queue_results(input_params, lock, results_q):
         lock.acquire()
@@ -159,16 +217,19 @@ if __name__ == "__main__":
 
     processes_list = []
 
-    for input_params in SPM_PARAMS_OBJ_L:
+    for input_params in DataProcessingSettings.spm_params_obj_l:
         p = Process(
             target=queue_results,
             args=(input_params, lock, results_q),
         )
         p.start()
         processes_list.append(p)
-    print("Processes started...", processes_list)
-    print("Processes started...", SPM_PARAMS_OBJ_L)
-    for _, (p, input_params) in enumerate(zip(processes_list, SPM_PARAMS_OBJ_L)):
+    if debug:
+        print("Debug: Processes started...", processes_list)
+        print("DEbug: Processes started...", DataProcessingSettings.spm_params_obj_l)
+    for _, (p, input_params) in enumerate(
+        zip(processes_list, DataProcessingSettings.spm_params_obj_l)
+    ):
         p.join(time_out)
         if p.is_alive():
             print(
@@ -179,14 +240,16 @@ if __name__ == "__main__":
 
     indices = []
     completed_param_objs = []
-    # TODO: Get the input parameters rom the queue, on which we can delete the raw files.
     while not results_q.empty():
+        # Get back input_params obj with output files
         completed_param_objs.append(results_q.get())
     upload_time_limit = datetime.now() + timedelta(seconds=time_out)
 
     while (
         len(completed_param_objs) > len(indices) and datetime.now() < upload_time_limit
     ):
+        sucess_ind = []
+        failed_ind = []
         for ind, complete_param_obj in enumerate(completed_param_objs):
             zip_to_upload = complete_param_obj.zip_file_path
             if ind in indices:  # already processed or failed
@@ -195,11 +258,27 @@ if __name__ == "__main__":
                 indices.append(ind)
                 continue
             indices.append(ind)
-            upload_id = upload_to_NOMAD(nomad_url, token, zip_to_upload)
-            print(f"Upload ID: {upload_id}")
+            massage = "Adding files"
+            max_attempt = 20
+            attempt = 0
+            try:
+                upload_id = upload_to_NOMAD(
+                    NOMADSettings.url, NOMADSettings.token, zip_to_upload
+                )
+                massage = check_upload_status(
+                    NOMADSettings.url, NOMADSettings.token, upload_id
+                )
+                print(f"Process Info: Upload ID: {upload_id}")
+                sucess_ind.append(ind)
+            except Exception as e:
+                print(f"Error in uploading {zip_to_upload}: {e}")
+                failed_ind.append(ind)
+                continue
             # To modify metadata
-            if modify_upload_metadata:
-                dataset_id = create_dataset(nomad_url, token, "Test_Dataset")
+            if NOMADSettings.modify_upload_metadata:
+                dataset_id = create_dataset(
+                    NOMADSettings.url, NOMADSettings.token, "Test_Dataset"
+                )
                 metadata = {
                     "metadata": {
                         "upload_name": "Test_Upload",
@@ -210,34 +289,50 @@ if __name__ == "__main__":
                         "comment": "This is a test upload...",
                     },
                 }
-                edit_upload_metadata(nomad_url, token, upload_id, metadata)
+                edit_upload_metadata(
+                    NOMADSettings.url, NOMADSettings.token, upload_id, metadata
+                )
 
-            if publish_to_nomad:
-                publish_upload(nomad_url, token, upload_id)
-            massage = "Adding files"
-            max_attempt = 20
-            attempt = 0
+            if NOMADSettings.publish_to_nomad:
+                publish_upload(NOMADSettings.url, NOMADSettings.token, upload_id)
+
             while (
                 not massage.startswith("Process process_upload completed successfully")
                 and attempt < max_attempt
             ):
-                # TODO: If upload is sucessfull, remove the zip and raw files
                 attempt += 1
-                massage = check_upload_status(nomad_url, token, upload_id)
+                massage = check_upload_status(
+                    NOMADSettings.url, NOMADSettings.token, upload_id
+                )
                 time.sleep(4 / 60)  # 4 second
 
-            print(f"Info: Upload status: {massage}")
-
-            if (mimic or not copy_file_elsewhere) and massage.startswith(
-                "Process process_upload completed successfully"
+            print(f"Process Info: Upload status: {massage}")
+        for ind, input_params in enumerate(completed_param_objs):
+            raw_file = list(
+                filter(
+                    lambda x: x.suffix in DataProcessingSettings.raw_file_exts,
+                    input_params.input_file,
+                )
+            )[0]
+            # Clean up the files generated during the process
+            if input_params.zip_file_path and input_params.zip_file_path.is_file():
+                input_params.zip_file_path.unlink()
+            if input_params.output and input_params.output.is_file():
+                input_params.output.unlink()
+            if (
+                DataProcessingSettings.create_pseudo_file
+                or not DataProcessingSettings.copy_file_elsewhere
             ):
-                raw_file = list(
-                    filter(
-                        lambda x: x.suffix in available_file_extensions,
-                        input_params.input_file,
-                    )
-                )[0]
-                # Track if upload is sucessully done
-                mimicked_file = raw_file.with_suffix(f"{raw_file.suffix}.done").touch()
+                # Track if upload is successfully done
+                pseudo_file = raw_file.with_suffix(
+                    f"{raw_file.suffix}{DataProcessingSettings.pseudo_exts}"
+                )
+                pseudo_file.touch()
+                # remove the mimicked file if upload is failed
+                if pseudo_file and ind in failed_ind:
+                    pseudo_file.unlink()
+            elif DataProcessingSettings.copy_file_elsewhere:
+                # As raw file stored in other location, remove it
+                raw_file.unlink()
 
-    print("All done!", indices)
+    print("Process Info: All done!")
