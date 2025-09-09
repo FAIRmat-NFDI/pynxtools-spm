e4bf3317ad9e42c9a394e9570664431112376d50
Rubel <rubel.mozumder@outlook.com>
2025-09-09 12:20:13 +0200

    cleaning up code.

--- src/pynxtools_spm/nxformatters/helpers.py
+++ src/pynxtools_spm/nxformatters/helpers.py
@@ -1,3 +1,23 @@
+"""Helpers for nxformatters."""
+
+# -*- coding: utf-8 -*-
+#
+# Copyright The NOMAD Authors.
+#
+# This file is part of NOMAD. See https://nomad-lab.eu for further info.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from __future__ import annotations
 import json
 import logging
 import re
@@ -5,7 +25,7 @@ import zoneinfo
 from copy import deepcopy
 from datetime import datetime
 from pathlib import Path
-from typing import Any, Callable, Dict, Literal, Optional, Tuple, Union
+from typing import Any, Callable, Dict, Optional, Tuple, Union
 
 import numpy as np
 import tzlocal
