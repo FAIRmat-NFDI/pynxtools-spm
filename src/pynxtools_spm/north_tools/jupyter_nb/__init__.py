2eedcc3ddbdfcc4a76595958a3df5f581ffb23d9
Rubel <rubel.mozumder@outlook.com>
2026-01-15 14:43:25 +0100

    Update fix file path.

---
+++ src/pynxtools_spm/north_tools/jupyter_nb/__init__.py
@@ -0,0 +1,24 @@
+from nomad.config.models.north import NORTHTool
+from nomad.config.models.plugins import NorthToolEntryPoint
+
+tool = NORTHTool(
+    short_description="Jupyter Notebook server for SPM techniques in NOMAD NORTH.",
+    # image='gitlab-registry.mpcdf.mpg.de/nomad-lab/north/xps:master',
+    image="ghcr.io/fairmat-nfdi/pynxtools-spm/jupyter:pr-70",
+    description="Jupyter Notebook server for SPM (Scanning Probe Microscopy)",
+    external_mounts=[],
+    file_extensions=["ipynb", "nxs", "h5", "hdf5"],
+    icon="logo/jupyter.svg",
+    image_pull_policy="Always",
+    default_url="/lab",
+    maintainer=[
+        {"email": "rubel.mozumder@physik.hu-berlin.de", "name": "Rubel Mozumder"}
+    ],
+    mount_path="/home/jovyan",
+    path_prefix="lab/tree",
+    privileged=False,
+    with_path=True,
+    display_name="SPM",
+)
+
+north_tool = NorthToolEntryPoint(id_url_safe="spm_jupyter_container", north_tool=tool)
