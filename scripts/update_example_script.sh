ff8d939459ec7d6fb5ad888a76d550446c2f00ff
Rubel <rubel.mozumder@outlook.com>
2026-08-24 17:07:18 +0200

    rebase.

--- scripts/update_example_script.sh
+++ scripts/update_example_script.sh
@@ -17,8 +17,8 @@ find ${root_dir}/tests/data/nanonis/sts/version_gen_5e_with_described_nxdata -ty
 find ${root_dir}/tests/data/nanonis/sts/version_gen_5_with_described_nxdata -type f ! -name '*.nxs' | xargs pynx convert --nxdl NXsts --reader spm --output sts_5_with_described_nxdata.nxs #--skip-verify
 find ${root_dir}/tests/data/nanonis/sts/version_gen_5_with_described_nxdata -type f -name '*.nxs' | xargs mv sts_5_with_described_nxdata.nxs
 
-# # STM Nanonis 5e and 5
-# echo " !!! Converting Nanonis STM data !!! "
+# STM Nanonis 5e and 5
+echo " !!! Converting Nanonis STM data !!! "
 
 find ${root_dir}/tests/data/nanonis/stm/version_gen_5e_with_described_nxdata -type f ! -name '*.nxs' | xargs pynx convert --nxdl NXstm --reader spm --output stm_5e_with_described_nxdata.nxs
 find ${root_dir}/tests/data/nanonis/stm/version_gen_5e_with_described_nxdata -type f -name '*.nxs' | xargs mv stm_5e_with_described_nxdata.nxs
